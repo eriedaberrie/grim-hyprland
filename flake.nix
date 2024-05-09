7fca5ead10c241ceba9b4f9bb7e93179e8280f2e
eriedaberrie <eriedaberrie@gmail.com>
2024-05-09 13:44:10 -0700

    nix: add nix-systems input

--- flake.nix
+++ flake.nix
@@ -1,25 +1,27 @@
 {
   description = "Grab images from a Wayland compositor (Hyprland fork)";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+  };
 
   outputs = {
     self,
     nixpkgs,
+    systems,
+    ...
   }: let
     inherit (nixpkgs) lib;
     forSystems = f:
-      lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-      ] (system: f nixpkgs.legacyPackages.${system});
+      lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
   in {
     overlays = {
       default = _: prev: rec {
         grim = prev.grim.overrideAttrs (old: {
           pname = "grim-hyprland";
           version = self.rev or "dirty";
-          src = prev.lib.cleanSource ./.;
+          src = lib.cleanSource ./.;
           patches = [];
         });
 
